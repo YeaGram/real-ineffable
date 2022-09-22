116528ecd9018b62d21cc78141ebaddd203a7588
YeaGram <managerdims@gmail.com>
2022-09-23 06:45:59 +0800

    fixing image

--- components/header/infinteBackgroundTiles/image.jsx
+++ components/header/infinteBackgroundTiles/image.jsx
@@ -4,7 +4,7 @@ export default function ImageTiles({ tilesIndex }) {
   return (
     <span className="relative  flex items-center content-center w-[460px] h-[270px]">
       <Image
-        src={`/background/palet${tilesIndex}.png `}
+        src={`/palet${tilesIndex}.PNG `}
         alt="img"
         layout="fill"
         objectFit="cover"
